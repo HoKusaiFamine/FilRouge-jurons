fb85f34e58afe56578937640252291924e0c0e40
59013-90-13 <59013-90-13@exchange.ad.afpanet>
2023-02-22 14:45:32 +0100

    mise a jour des fichiers

---
+++ TP-11 FINAL/Controller/executeUser.php
@@ -0,0 +1,10 @@
+<?php
+include_once "../modele/Services/DBManagement.php";
+include_once "../modele/Services/PenaliteServices.php";
+
+
+insertStagiaires("rachinas","thomas","thomas0205","12335",1);
+
+
+
+?>
