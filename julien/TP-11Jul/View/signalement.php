3748c067ad7345c14144abfed836a6e8d29d29ac
59013-90-17 <59013-90-17@exchange.ad.afpanet>
2023-02-23 16:19:07 +0100

    envoi pages julien

--- julien/TP-11Jul/View/signalement.php
+++ julien/TP-11Jul/View/signalement.php
@@ -1,12 +1,12 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="fr">
 
 <head>
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
-  <link rel="stylesheet" href="signalement.css">
+  <link rel="stylesheet" href="CSS/signalement.css">
   <title>Document</title>
 </head>
 
@@ -14,7 +14,7 @@
 
   <?php
   session_start();
-  /* $listeStag = array("Kaouthar",
+  /*$listeStag = array("Kaouthar",
   "Lucas",
   "Sebastien",
   "Yacoube",
@@ -29,8 +29,8 @@
   "Florian",
   "Paul",
   "Batiste");
-   $listepenalite = */
-  print_r($_SESSION["nom"]) ?>
+    $listepenalite = */
+  //print_r($_SESSION["nom"]) ?>
 <div class="card" style="width: 40rem;height: 30rem">
 <div class="card-body">
 
